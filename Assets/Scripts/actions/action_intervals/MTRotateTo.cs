41fd3333caa3229aa2485a1a3defb552e76219de
jyzgo <jiangyizheng@magictavern.com>
2015-11-05 16:10:30 +0800

    修复了Rotateto

--- Assets/Scripts/actions/action_intervals/MTRotateTo.cs
+++ Assets/Scripts/actions/action_intervals/MTRotateTo.cs
@@ -6,22 +6,16 @@ namespace MTUnity.Actions
 {
     public class MTRotateTo : MTFiniteTimeAction
     {
-        public float DistanceAngleX { get; private set; }
-        public float DistanceAngleY { get; private set; }
-		public float DistanceAngleZ { get; private set; }
-
 
         #region Constructors
+    
+        public float Duration{get;private set;}
+        public Vector3 TargetAngle{get;private set;}
 
-		public MTRotateTo (float duration, float deltaAngleX, float deltaAngleY,float deltaAngleZ) : base (duration)
+        public MTRotateTo(float duration,Vector3 toAngle):base(duration)
         {
-            DistanceAngleX = deltaAngleX;
-            DistanceAngleY = deltaAngleY;
-			DistanceAngleZ = deltaAngleZ;
-        }
-
-		public MTRotateTo (float duration, float deltaAngle) : this (duration, deltaAngle, deltaAngle,deltaAngle)
-        {
+            Duration = duration;
+            TargetAngle = toAngle;
         }
 
         #endregion Constructors
@@ -40,103 +34,29 @@ namespace MTUnity.Actions
 
     public class MTRotateToState : MTFiniteTimeActionState
     {
-        protected float DiffAngleY;
-        protected float DiffAngleX;
-		protected float DiffAngleZ;
 
-        protected float DistanceAngleX { get; set; }
-
-        protected float DistanceAngleY { get; set; }
-
-		protected float DistanceAngleZ { get ; set;}
-
-        protected float StartAngleX;
-        protected float StartAngleY;
-		protected float StartAngleZ;
 
         public MTRotateToState (MTRotateTo action, GameObject target)
             : base (action, target)
         { 
-            DistanceAngleX = action.DistanceAngleX;
-            DistanceAngleY = action.DistanceAngleY;
-			DistanceAngleZ = action.DistanceAngleZ;
 
-            // Calculate X
-			StartAngleX = Target.transform.localRotation.x;
-            if (StartAngleX > 0)
-            {
-                StartAngleX = StartAngleX % 360.0f;
-            }
-            else
-            {
-                StartAngleX = StartAngleX % -360.0f;
-            }
-
-            DiffAngleX = DistanceAngleX - StartAngleX;
-            if (DiffAngleX > 180)
-            {
-                DiffAngleX -= 360;
-            }
-            if (DiffAngleX < -180)
-            {
-                DiffAngleX += 360;
-            }
-
-            //Calculate Y: It's duplicated from calculating X since the rotation wrap should be the same
-			StartAngleY = Target.transform.localRotation.y;
-
-            if (StartAngleY > 0)
-            {
-                StartAngleY = StartAngleY % 360.0f;
-            }
-            else
-            {
-                StartAngleY = StartAngleY % -360.0f;
-            }
-
-            DiffAngleY = DistanceAngleY - StartAngleY;
-            if (DiffAngleY > 180)
-            {
-                DiffAngleY -= 360;
-            }
-
-            if (DiffAngleY < -180)
-            {
-                DiffAngleY += 360;
-            }
-
-			StartAngleZ = Target.transform.localRotation.z;
-
-			if (StartAngleZ > 0)
-			{
-				StartAngleZ = StartAngleZ % 360.0f;
-			}
-			else
-			{
-				StartAngleZ = StartAngleZ % -360.0f;
-			}
-
-			DiffAngleZ = DistanceAngleZ - StartAngleY;
-			if (DiffAngleY > 180)
-			{
-				DiffAngleY -= 360;
-			}
-
-			if (DiffAngleY < -180)
-			{
-				DiffAngleY += 360;
-			}
+            FromAngle = Target.transform.localRotation;
+            ToAngle = Quaternion.Euler( action.TargetAngle);
+            InTime = action.Duration;
         }
+
+        Quaternion FromAngle;
+        Quaternion ToAngle;
+        float InTime;
+        float curTime = 0f;
 
         public override void Update (float time)
         {
-            if (Target != null)
-            {
-                var RotationX = StartAngleX + DiffAngleX * time;
-                var RotationY = StartAngleY + DiffAngleY * time;
-				var RotationZ = StartAngleZ + DiffAngleZ * time;
-				Target.transform.Rotate (RotationX, RotationY, RotationZ);
-            }
+
+            Target.transform.localRotation = Quaternion.Lerp(FromAngle,ToAngle,curTime/InTime);
+
+            curTime += Time.deltaTime;
+
         }
 
     }
