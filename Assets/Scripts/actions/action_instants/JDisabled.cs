331c0ff1ebc8a4268043bc4d3be9c1848b7fb534
jyzHome <jyzgo@163.com>
2019-06-08 12:14:35 +1000

    Add JEnable and JDisable

---
+++ Assets/Scripts/actions/action_instants/JDisabled.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace JUnity.Actions
+{
+    public class JDisabled: JActionInstant
+    {
+        #region Constructors
+
+        public  JDisabled()
+        {
+        }
+
+        #endregion Constructors
+
+
+        protected internal override JActionState StartAction(GameObject target)
+        {
+            return new JDisabledState(this, target);
+
+        }
+
+        public override JFiniteTimeAction Reverse ()
+        {
+            return (new JShow ());
+        }
+
+    }
+
+    public class JDisabledState: JActionInstantState
+    {
+
+        public JDisabledState(JDisabled action, GameObject target)
+            : base (action, target)
+        {
+            if (target != null)
+            {
+                target.SetActive(false);
+            }
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/actions/action_instants/JEnabled.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace JUnity.Actions
+{
+    public class JEnabled: JActionInstant
+    {
+     
+
+        protected internal override JActionState StartAction(GameObject target)
+        {
+            return new JEnabledState(this, target);
+
+        }
+
+        public override JFiniteTimeAction Reverse ()
+        {
+            return (new JShow ());
+        }
+
+    }
+
+    public class JEnabledState: JActionInstantState
+    {
+
+        public JEnabledState(JEnabled action, GameObject target)
+            : base (action, target)
+        {
+            if (target != null)
+            {
+                target.SetActive(true);
+            }
+        }
+
+    }
+
+}
